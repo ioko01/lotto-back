63ab7bb811d5096202ecd17e1d58c99e0c7caf87
unknown <ioko.peezaza@gmail.com>
2024-01-23 22:34:17 +0700

    commit

--- src/routes/file.ts
+++ src/routes/file.ts
@@ -18,7 +18,7 @@ export class ApiFile {
                             const file = req.files.File as UploadedFile
                             const filename = file.name
                             if (file.mimetype.match("image/")) {
-                                file.mv(`./src/uploads/${utf8.decode(filename)}`, (err) => {
+                                file.mv(`./src/uploads/${utf8.decode(filename).trim()}`, (err) => {
                                     if (err) return res.send(err)
                                     return res.send("File Uploaded")
                                 })
