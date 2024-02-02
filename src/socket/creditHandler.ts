c7f3a70844d45b8965d742b4869bb5fccaf9000a
Your Name <ioko.peezaza@gmail.com>
2024-02-02 16:13:38 +0700

    commit

--- src/socket/creditHandler.ts
+++ src/socket/creditHandler.ts
@@ -7,6 +7,7 @@ export const creditHandler = (socket: Socket<ClientToServerEvents, ServerToClien
 
     const createCredit = () => {
         io.emit("get_credit")
+        console.log("get_credit");
     }
 
     socket.on("create_credit", createCredit)
